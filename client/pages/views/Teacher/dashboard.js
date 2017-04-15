79190dcd4972599ffc2ef181e99fca5761bbb500
charbencayetano <charbcay@gmail.com>
2017-04-15 15:39:30 -0700

    CMC

--- client/pages/views/Teacher/dashboard.js
+++ client/pages/views/Teacher/dashboard.js
@@ -1,12 +1,19 @@
+import Chart from 'chart.js'
+
 if(Meteor.isClient) {
+
     Template.dashboard.onCreated(function () {
 
     });
     Template.dashboard.onRendered(function () {
-
+        getChart();
+        getRadarChart();
+        getLineChart();
     });
     Template.dashboard.helpers({
-
+        "todayDate":function(){
+            return moment(Date.now()).format("dddd, MMMM Do YYYY, h:mm:ss a");
+        }
     });
     Template.dashboard.events({
 
@@ -15,3 +22,110 @@ if(Meteor.isClient) {
 
     });
 }
+function getChart(){
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Ques1", "Ques2", "Ques3", "Ques4", "Ques5", "Ques6"],
+            datasets: [{
+                label: '# of Student',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+}
+function getLineChart(){
+    var ctx = document.getElementById("myLineChart");
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: data2,
+        options: {}
+    });
+}
+function getRadarChart() {
+    var ctx = document.getElementById("myRadarChart");
+    var myRadarChart = new Chart(ctx, {
+        type: 'radar',
+        data: data,
+        options: {}
+    });
+}
+var data = {
+    labels: ["Easy", "Intermediate", "Hard", "listening", "Video"],
+    datasets: [
+        {
+            label: "My First Test",
+            backgroundColor: "rgba(179,181,198,0.2)",
+            borderColor: "rgba(179,181,198,1)",
+            pointBackgroundColor: "rgba(179,181,198,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(179,181,198,1)",
+            data: [65, 59, 90, 81, 56]
+        },
+        {
+            label: "My Second Test",
+            backgroundColor: "rgba(255,99,132,0.2)",
+            borderColor: "rgba(255,99,132,1)",
+            pointBackgroundColor: "rgba(255,99,132,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(255,99,132,1)",
+            data: [28, 48, 40, 19, 86]
+        }
+    ]
+};
+var data2 = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "Student Taken Test",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+            spanGaps: false,
+        }
+    ]
+};
--- client/pages/views/Teacher/testList.js
+++ client/pages/views/Teacher/testList.js
@@ -1,17 +1,105 @@
 if(Meteor.isClient) {
     Template.testList.onCreated(function () {
-
+        Meteor.subscribe("testList");
+/*
+        _.each(TestList,function(element){
+            TestListCollection.insert(element);
+         });
+        console.log(TestListCollection.find().fetch());
+*/
     });
     Template.testList.onRendered(function () {
 
     });
     Template.testList.helpers({
-
+        "TestList":function() {
+            return TestListCollection.find({}).fetch();
+        }
     });
     Template.testList.events({
-
+        "click .glyphicon-eye-open":function(e){
+            FlowRouter.go('/teacher/preview?id=' + this._id);
+        },
+        "click .glyphicon-envelope":function(){
+            console.log(this._id);
+            $('#myModalInvitation').modal();
+        }
     });
     Template.testList.onDestroyed(function(){
 
     });
 }
+
+
+var TestList=[
+    {
+        "Title": "English",
+        "DateCreated": "02/09/2016 10:30 am",
+        "DateUpdated": "02/10/2016 11:45 am",
+        "TotalStudent": 25,
+        "TotalStudentTaken" : 1
+    },
+    {
+        "Title": "English",
+        "DateCreated": "02/09/2016 10:30 am",
+        "DateUpdated": "02/10/2016 11:45 am",
+        "TotalStudent": 35,
+        "TotalStudentTaken" : 10
+    },
+    {
+        "Title": "English",
+        "DateCreated": "02/09/2016 10:30 am",
+        "DateUpdated": "02/10/2016 11:45 am",
+        "TotalStudent": 15,
+        "TotalStudentTaken" : 5
+    },
+    {
+        "Title": "English",
+        "DateCreated": "02/09/2016 10:30 am",
+        "DateUpdated": "02/10/2016 11:45 am",
+        "TotalStudent": 9,
+        "TotalStudentTaken" : 9
+    },
+    {
+        "Title": "English",
+        "DateCreated": "02/09/2016 10:30 am",
+        "DateUpdated": "02/10/2016 11:45 am",
+        "TotalStudent": 15,
+        "TotalStudentTaken" : 11
+    },
+    {
+        "Title": "English",
+        "DateCreated": "02/09/2016 10:30 am",
+        "DateUpdated": "02/10/2016 11:45 am",
+        "TotalStudent": 22,
+        "TotalStudentTaken" : 13
+    },
+    {
+        "Title": "English",
+        "DateCreated": "02/09/2016 10:30 am",
+        "DateUpdated": "02/10/2016 11:45 am",
+        "TotalStudent": 25,
+        "TotalStudentTaken" : 25
+    },
+    {
+        "Title": "English",
+        "DateCreated": "02/09/2016 10:30 am",
+        "DateUpdated": "02/10/2016 11:45 am",
+        "TotalStudent": 18,
+        "TotalStudentTaken" : 14
+    },
+    {
+        "Title": "English",
+        "DateCreated": "02/09/2016 10:30 am",
+        "DateUpdated": "02/10/2016 11:45 am",
+        "TotalStudent": 25,
+        "TotalStudentTaken" : 10
+    },
+    {
+        "Title": "English",
+        "DateCreated": "02/09/2016 10:30 am",
+        "DateUpdated": "02/10/2016 11:45 am",
+        "TotalStudent": 5,
+        "TotalStudentTaken" : 5
+    }
+]
